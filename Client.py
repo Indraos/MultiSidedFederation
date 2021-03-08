35411921c06ea9acab03aee9f017633d26a9c677
Andreas Haupt <a.haupt@me.com>
2021-03-08 18:27:30 -0500

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,40 @@
+class Client:
+    def __init__():
+        pass
+
+    def send_models():
+
+
+class DemandClient(Client):
+    def __init__(self, train_dl, optimizer, test_dl, criterion, initial_model):
+        self.receivers = []
+
+    def aggregate(models):
+        return model
+
+    def evaluate(model):
+        pass
+
+    def bid(bid: float):
+        self._bid = bid
+
+    def pay(amount):
+        self.pay += amount
+
+    def send_models():
+        return model
+
+
+class SupplyClient(Client):
+    def __init__(self, test_dl, criterion, initial_model):
+        self.test_dl = test_dl
+        self.criterion = criterion
+        self.receivers = []
+
+    def train():
+        return model
+
+    def send_models():
+        return model
+
+
